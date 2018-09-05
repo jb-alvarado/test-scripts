06f601a767c47c1a3c0457aefad32f28334197af
jb-alvarado <jb@pixelcrusher.de>
2018-09-05 17:43:02 +0200

    write decoded frames only to one queue

--- test_scripts/player.py
+++ test_scripts/player.py
@@ -10,14 +10,14 @@ currentdir = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.abspath(os.path.join(currentdir, os.pardir))
 sys.path.insert(0, parentdir)
 
-from decoder import Decode, V_FIFO, A_FIFO
+from decoder import Decode
 
 decode = Decode(
     ['test_clips/test.mp4',
-     'test_clips/test.mp4',
-     'test_clips/test.mp4',
+     'test_clips/waves.mp4',
+     'test_clips/water.mp4',
      'test_clips/test.mp4'
-     ], 1024, 576)
+     ], 1024, 576, 44100)
 
 decode.setDaemon(True)
 decode.start()
@@ -28,23 +28,23 @@ play = None
 
 while True:
     try:
-        v = V_FIFO.get(block=True, timeout=0.7)
-        a = A_FIFO.get(block=True, timeout=0.7)
+        frame = decode.fifo.get(block=True, timeout=0.8)
         if not play:
             cmd = [
                 'ffplay',
-                '-f', 'rawvideo',
-                '-pixel_format', 'rgb24',
-                '-video_size', '1024x576',
-                # '-f', 's16le',
-                # '-ar', '44100',
-                # '-ac', '2',
+                # '-f', 'rawvideo',
+                # '-pixel_format', 'rgb24',
+                # '-video_size', '1024x576',
+                '-f', 's16le',
+                '-ar', '44100',
+                '-ac', '2',
                 '-i', 'pipe:0',
                 ]
             play = Popen(cmd, stdin=PIPE)
         try:
-            play.stdin.write(v.to_bytes())
-            sleep(0.04)
+            if frame[1] is not None:
+                play.stdin.write(frame[1].to_bytes())
+            # sleep(0.04)
             # play.stdin.write(a.to_bytes())
         except IOError as e:
             print(e)
