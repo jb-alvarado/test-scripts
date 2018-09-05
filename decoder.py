e881ed6616828fa1780ded44956af98e04e8d169
jb-alvarado <jb@pixelcrusher.de>
2018-09-05 17:42:06 +0200

    write decoded frame only to one queue

--- decoder.py
+++ decoder.py
@@ -1,55 +1,67 @@
-# This file is part of ffplayout
+# This file is part of ffplayout-engine
 #
-# ffplayout is free software: you can redistribute it and/or modify
+# ffplayout-engine is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ffplayout is distributed in the hope that it will be useful,
+# ffplayout-engine is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ffplayout. If not, see <http://www.gnu.org/licenses/>.
+# along with ffplayout-engine. If not, see <http://www.gnu.org/licenses/>.
 
 # ------------------------------------------------------------------------------
 
-
-import av
 from queue import Queue
 from threading import Thread
 
-V_FIFO = Queue(maxsize=300)
-A_FIFO = Queue(maxsize=300)
+import av
 
 
 # main decoding thread
 class Decode(Thread):
-    def __init__(self, input, width, height):
+    def __init__(self, input, width, height, audio_rate=44100):
         Thread.__init__(self)
         self.input = input
         self.w = width
         self.h = height
+        self.audio_rate = audio_rate
 
-        self.resampler = av.AudioResampler(
-            format=av.AudioFormat('s16'),
-            layout=2,
-            rate=44100,
-        )
+        # set global fifo buffer
+        # it must hold the content while the loop runs over the input files
+        self.fifo = Queue(maxsize=100)
 
     def run(self):
         for input in self.input:
             container = av.open(input, 'r')
 
+            # resample audio line to the given format
+            resampler = av.AudioResampler(
+                format=av.AudioFormat('s16'),
+                layout=2,
+                rate=self.audio_rate,
+            )
+
+            # loop over the container
             for packet in container.demux():
-                if packet.stream.type == 'video':
-                    for v_frame in packet.decode():
-                        v_frame.pts = None
-                        new_v_frame = v_frame.reformat(self.w, self.h, 'rgb24')
-                        V_FIFO.put(new_v_frame.planes[0])
-                if packet.stream.type == 'audio':
-                    for a_frame in packet.decode():
-                        a_frame.pts = None
-                        new_a_frame = self.resampler.resample(a_frame)
-                        A_FIFO.put(new_a_frame.planes[0])
+                type = packet.stream.type
+
+                for frame in packet.decode():
+                    video_frame = None
+                    audio_frame = None
+
+                    if type == 'video':
+                        frame.pts = None
+                        new_v_frame = frame.reformat(self.w, self.h, 'rgb24')
+                        video_frame = new_v_frame.planes[0]
+
+                    if type == 'audio':
+                        frame.pts = None
+                        new_a_frame = resampler.resample(frame)
+                        audio_frame = new_a_frame.planes[0]
+
+                    # push to fifo buffer
+                    self.fifo.put([video_frame, audio_frame])
