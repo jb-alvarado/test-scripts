29d7ec137054421506d0c49a15f317b0c67ab8e7
jb-alvarado <jb@pixelcrusher.de>
2018-11-29 12:11:39 +0100

    test scripts

--- test_scripts/concat.py
+++ test_scripts/concat.py
@@ -1,6 +1,7 @@
 #!/Users/jonathan/dev/GIT/github/ffplayout-engine/venv/bin/python3
 
 import os
+import datetime
 
 import av
 
@@ -9,14 +10,18 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 output = av.open('concat.mkv', 'w')
 
 out_stream_v = output.add_stream('libx264', 25)
-out_stream_a = output.add_stream('aac')
+out_stream_a = output.add_stream('aac', 44100)
 out_stream_v.width = 1024
 out_stream_v.height = 576
-out_stream_v.options = {'preset': 'faster', 'crf': '24'}
+out_stream_v.options = {'preset': 'faster', 'crf': '23'}
 out_stream_a.options = {'b': '128k'}
 
 
 list = [
+    'test.mp4',
+    'test.mp4',
+    'test.mp4',
+    'test.mp4',
     'test.mp4',
     'test.mp4',
 ]
@@ -24,15 +29,11 @@ list = [
 new_video_pts = 0
 new_audio_pts = 0
 
+print(str(datetime.datetime.now()))
+
 for clip in list:
     input_ = av.open(os.path.join(dir_path, 'test_clips', clip))
     print(input_)
-
-    resampler = av.AudioResampler(
-        format=av.AudioFormat('s16'),
-        layout=2,
-        rate=44100,
-    )
 
     in_stream_v = input_.streams.video[0]
     in_stream_a = input_.streams.audio[0]
@@ -52,7 +53,6 @@ for clip in list:
                 new_video_pts += 512
 
             elif type == 'audio':
-                # new_a_frame = resampler.resample(frame)
                 frame.pts = new_audio_pts
                 packet = out_stream_a.encode(frame)
                 new_audio_pts += 1024
@@ -60,3 +60,5 @@ for clip in list:
             output.mux(packet)
 
 output.close()
+
+print(str(datetime.datetime.now()))
